4f73dbfe37b0584cf9568359647b9bb4021a3add
Colin Khoury <c.khoury@cgiar.org>
2020-05-13 16:06:27 -0600

    Update data.R

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #' @name CucurbitaData
 #' @docType data
 #' @description This dataset is a subset of the original dataset for:
-#'  C. cordata, C. digitata and C. palmata used in Khoury et al., 2020
+#'  C. cordata, C. digitata and C. palmata used in Khoury et al. (2019)
 #' @format A data frame with 1184 rows and 4 variables:
 #' \describe{
 #'   \item{taxon}{character: Species name}
@@ -11,9 +11,7 @@
 #'   \item{type}{character: Source of the record,germplasm (G) or herbarium (H)}
 #' }
 #' @references
-#' Khoury, CK, Carver, D, Kates, HR, et al. Distributions, conservation status,
-#' and abiotic stress tolerance potential of wild cucurbita (Cucurbita L.).
-#' Plants, People, Planet. 2019; 00: 1– 15. https://doi.org/10.1002/ppp3.10085
+#' Khoury et al. (2019) Plants, People, Planet 2(3):269-283. doi: 10.1002/ppp3.10085. https://doi.org/10.1002/ppp3.10085
 #' @source \url{https://dataverse.harvard.edu/dataverse/GapAnalysisR}
 "CucurbitaData"
 
@@ -23,10 +21,6 @@
 #' @format raster files
 #' @description This dataset is a subset of species distribution models for:
 #'  C. cordata, C. digitata and C. palmata used in Khoury et al., 2020
-#' @references
-#' Khoury, CK, Carver, D, Kates, HR, et al. Distributions, conservation status,
-#'  and abiotic stress tolerance potential of wild cucurbita (Cucurbita L.).
-#'  Plants, People, Planet. 2019; 00: 1– 15. https://doi.org/10.1002/ppp3.10085
 #' @source \url{https://dataverse.harvard.edu/dataverse/GapAnalysisR}
 "CucurbitaRasters"
 
@@ -44,10 +38,6 @@
 #' @docType data
 #' @format Raster file
 #' @description This dataset is a raster version of the world protected areas dataset
-#'  used in Khoury et al., (2019)
+#'  used in Khoury et al. (2019)
 #' @source \url{https://www.protectedplanet.net/}
-#' @references
-#' Khoury, C. K., Amariles, D., Soto, J. S., Diaz, M. V., Sotelo, S., Sosa, C. C., … Jarvis, A. (2019).
-#' Comprehensiveness of conservation of useful wild plants: An operational indicator for biodiversity
-#' and sustainable development targets. Ecological Indicators. https://doi.org/10.1016/j.ecolind.2018.11.016
 "ProtectedAreas"
