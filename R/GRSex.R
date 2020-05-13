a6faf2a50aec3748176aa9fdd5255618f58a2384
Colin Khoury <c.khoury@cgiar.org>
2020-05-13 15:30:41 -0600

    Update GRSex.R

--- R/GRSex.R
+++ R/GRSex.R
@@ -1,18 +1,17 @@
-#' @title Geographical representativeness score estimation (Ex-situ conservation)
+#' @title Geographical representativeness score ex-situ
 #' @name GRSex
-#' @description This function performs an estimation of the geographical representativeness score
-#'  for ex-situ gap analysis (GRSex) using Ramirez-Villegas et al., (2010) methodology.
-#'  GRS ex-situ score is calculated as:
-#'
-#' \deqn{GRSex = min(100,(Masked Area of Buffered G Occurrences / Total Area of Predicted Habitat)*100)}
+#' @description The GRSex process provides a geographic measurement of the proportion of a species’ range 
+#' that can be considered to be conserved in ex situ repositories. The GRSex uses buffers (default 50 km radius) 
+#' created around each G coordinate point to estimate geographic areas already well collected within the distribution 
+#' models of each taxon, and then calculates the proportion of the distribution model covered by these buffers. 
 #'
 #' @param Occurrence_data A data frame object with the species name, geographical coordinates,
 #' and type of records (G or H) for a given species
-#' @param Species_list An species list to calculate the GRSex metrics.
+#' @param Species_list A species list to calculate the GRSex metrics.
 #' @param Raster_list A list representing the species distribution models for the species list provided
 #'  loaded in raster format. This list must match the same order of the species list.
 #' @param Buffer_distance Geographical distance used to create circular buffers around germplasm.
-#'  Default: 50000 that is 50 km around germplasm accessions (CA50)
+#'  Default: 50000 (50 km) around germplasm accessions (CA50)
 #'
 #' @return This function returns a data frame with two columns:
 #'
@@ -35,17 +34,8 @@
 #'                     Buffer_distance = 50000)
 #'
 #' @references
-#' Ramirez-Villegas, J., Khoury, C., Jarvis, A., Debouck, D. G., & Guarino, L. (2010).
-#' A Gap Analysis Methodology for Collecting Crop Genepools: A Case Study with Phaseolus Beans.
-#' PLOS ONE, 5(10), e13497. Retrieved from https://doi.org/10.1371/journal.pone.0013497
-#'
-#' Khoury, C. K., Amariles, D., Soto, J. S., Diaz, M. V., Sotelo, S., Sosa, C. C., … Jarvis, A. (2019).
-#' Comprehensiveness of conservation of useful wild plants: An operational indicator for biodiversity
-#' and sustainable development targets. Ecological Indicators. https://doi.org/10.1016/j.ecolind.2018.11.016
-#'
-#'
-#' Hijmans, R.J. and Spooner, D.M. (2001). Geographic distribution of wild potato species.
-#' Am. J. Bot., 88: 2101-2112. doi:10.2307/3558435
+#' Ramirez-Villegas et al. (2010) PLOS ONE, 5(10), e13497. doi: 10.1371/journal.pone.0013497
+#' Khoury et al. (2019) Ecological Indicators 98:420-429. doi: 10.1016/j.ecolind.2018.11.016
 #'
 #' @export
 #' @importFrom sp coordinates proj4string SpatialPoints over CRS
